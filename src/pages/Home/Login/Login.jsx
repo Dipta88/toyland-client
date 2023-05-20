28b6a8148bc75d54c4becdf0380a7a92127f4e0a
Dipta88 <user.name>
2023-05-20 14:21:03 +0600

    added firebase

--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleLogin = event =>{
+
+      event.preventDefault();  
+    }
     return (
         <div>
         <div className="hero min-h-screen bg-base-200">
@@ -13,24 +18,30 @@ const Login = () => {
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-warning">Login</button>
-        </div>
+      <form onSubmit={handleLogin}>
+      
+      <div className="form-control">
+      <label className="label">
+        <span className="label-text">Email</span>
+      </label>
+      <input type="text" placeholder="email" className="input input-bordered" />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Password</span>
+      </label>
+      <input type="text" placeholder="password" className="input input-bordered" />
+      <label className="label">
+        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+      </label>
+    </div>
+    <div className="form-control mt-6">
+      
+      <input className="btn btn-warning" type="submit" value="Login"/>
+    </div>
+      
+      
+      </form>
       </div>
     </div>
   </div>
